04fb96e956282b147d4aa17b8e43df6bba247ff1
Claire <claire.hw.cho@gmail.com>
2022-12-15 00:19:16 -0800

    algo

--- algo_answers.js
+++ algo_answers.js
@@ -1,26 +1,3 @@
-function missingValue (unorderedNums) {
-    //Your code here
-    let expected = 0;
-    let sum = 0;
-    let min = unorderedNums[0];
-    let max = unorderedNums[0];
-
-    for (let num of unorderedNums) {
-        sum = sum + num
-        //sum += num
-        if (min > num) min = num
-        if (max < num) max = num
-    }
-    for (let i = min; i <= max; i++) {
-        expected += i;
-    }
-    if (expected == sum) {
-        return null
-    }
-    let difference = expected - sum
-    return difference
-}
-
 // 7-22
 function mode (nums) {
     let obj = {}
